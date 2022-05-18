46de5915cf9b541222e9d378f713194e24e48f90
shamank <russyakrusya@gmail.com>
2022-05-18 23:52:21 +0300

    init commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Single from '../views/Single.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/:id',
+    name: 'single',
+    component: Single
   },
   {
     path: '/about',
